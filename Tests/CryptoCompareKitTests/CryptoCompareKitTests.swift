af4a0e7c012957d642d1692dc9122319b3c13e52
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2020-08-23 10:23:30 +0300

    refactor: remove commented code

--- Tests/CryptoCompareKitTests/CryptoCompareKitTests.swift
+++ Tests/CryptoCompareKitTests/CryptoCompareKitTests.swift
@@ -2,12 +2,7 @@ import XCTest
 @testable import CryptoCompareKit
 
 final class CryptoCompareKitTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(CryptoCompareKit().text, "Hello, World!")
-    }
+    func testExample() {}
 
     static var allTests = [
         ("testExample", testExample),
