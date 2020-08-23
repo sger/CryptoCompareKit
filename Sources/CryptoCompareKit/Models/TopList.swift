9f6d2cc30813bfcf86c2d217fbfdb72403454514
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2020-08-23 10:43:52 +0300

    feat: add initial values to top list struct

--- Sources/CryptoCompareKit/Models/TopList.swift
+++ Sources/CryptoCompareKit/Models/TopList.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 public struct TopList: Codable {
-    public let message: String
-    public let type: Int
-    public let data: [TopListData]
-    public let hasWarning: Bool
+    public let message: String = ""
+    public let type: Int = 0
+    public let data: [TopListData] = []
+    public let hasWarning: Bool = false
 
     enum CodingKeys: String, CodingKey {
         case message = "Message"
